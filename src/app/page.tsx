22718cad9d755deb92abeac490585d9eefdc591c
Oryvaio <bokolikowi@gmail.com>
2026-06-20 13:02:23 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import BorrowCard from "@/components/BorrowCard";
+import Analytics from "@/components/Analytics";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <BorrowCard />
+      <Analytics />
+    </>
+  );
+}
